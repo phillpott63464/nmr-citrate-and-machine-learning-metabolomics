d704b076100fa3da43eadcd9fcff87aa488472e6
sp970 <sp970@kent.ac.uk>
2025-07-25 16:18:40 +0100

    Added gaussian noise to spectra, working on model

--- NMR Sim Test.py
+++ NMR Sim Test.py
@@ -46,10 +46,10 @@ def _(mo):
 def _(vinyl):
     from nmrsim import SpinSystem
 
-    system = SpinSystem(vinyl[0], vinyl[1])
+    vinylsystem = SpinSystem(vinyl[0], vinyl[1])
 
-    system.peaklist()
-    return SpinSystem, system
+    vinylsystem.peaklist()
+    return SpinSystem, vinylsystem
 
 
 @app.cell(hide_code=True)
@@ -59,10 +59,10 @@ def _(mo):
 
 
 @app.cell
-def _(system):
+def _(vinylsystem):
     from nmrsim.plt import mplplot
 
-    mplplot(system.peaklist(), y_max=0.2)
+    mplplot(vinylsystem.peaklist(), y_max=0.2)
     return (mplplot,)
 
 
@@ -98,7 +98,7 @@ def _(np):
 
 @app.cell
 def _(SpinSystem, citrate):
-    citratesystem = SpinSystem(citrate[0], citrate[1])
+    citratesystem = SpinSystem(citrate[0], citrate[1], )
 
     citratesystem.peaklist()
     return (citratesystem,)
@@ -117,11 +117,201 @@ def _(mo):
 
 
 @app.cell
-def _(citratesystem, mplplot, system):
+def _(citratesystem, mplplot, vinylsystem):
     from nmrsim import Spectrum
 
-    mix = Spectrum([citratesystem, system])
+    mix = Spectrum([citratesystem, vinylsystem])
     mplplot(mix.peaklist())
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""Extract data of citrate graph into x, y""")
+    return
+
+
+@app.cell
+def _(citratesystem, mplplot):
+    import matplotlib.pyplot as plt
+
+    x, y = mplplot(citratesystem.peaklist(), y_max=0.4)
+
+    return plt, x, y
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""Create 100 noisy spectra""")
+    return
+
+
+@app.cell
+def _(np, plt, x, y):
+    ys = [y] * 100
+
+
+    for i in range(0, len(ys)):
+        sigma = np.random.rand() / 100
+        noise = np.random.normal(0.0, sigma, len(ys[0]))
+        ys[i] = y + noise
+    
+    plt.plot(x, ys[10])
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r""" """)
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""Create a spectra creator function""")
+    return
+
+
+@app.cell
+def _(mplplot, np):
+    def spectra_creator(plot):
+        x, y = mplplot(plot.peaklist(), y_max=0.4)
+
+        ys = [y] * 100
+    
+        for i in range(0, len(ys)):
+            sigma = np.random.rand() / 100
+            noise = np.random.normal(0.0, sigma, len(ys[0]))
+            ys[i] = y + noise
+
+        spectra = [[x, y] for y in ys]
+        return spectra
+    return (spectra_creator,)
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""Create 100 noisy spectra for both citrate and vinyl""")
+    return
+
+
+@app.cell
+def _(citratesystem, plt, spectra_creator, vinylsystem):
+    citrate_spectra = spectra_creator(citratesystem)
+    vinyl_spectra = spectra_creator(vinylsystem)
+
+    plt.plot(citrate_spectra[0][0], citrate_spectra[0][1])
+    return citrate_spectra, vinyl_spectra
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(
+        r"""
+    And now create a torch model to classify between the two?
+
+    """
+    )
+    return
+
+
+@app.cell
+def _():
+    import torch.nn as nn
+
+    # Define the model
+    model = nn.Sequential(
+        nn.Linear(8, 24),
+        nn.ReLU(),
+        nn.Linear(24, 12),
+        nn.ReLU(),
+        nn.Linear(12, 6),
+        nn.ReLU(),
+        nn.Linear(6, 1),
+    )
+    return (model,)
+
+
+@app.cell(hide_code=True)
+def _(citrate_spectra, np, vinyl_spectra):
+    import torch
+    from sklearn.model_selection import train_test_split
+
+
+
+    data = np.concatenate([citrate_spectra, vinyl_spectra], axis=0)
+
+    labels = np.concatenate([
+        np.zeros(100),
+        np.ones(100)
+    ])
+
+    train_ratio = 0.7   # 70% of the data is used for training, 30% for testing
+
+    data_train, data_test, labels_train, labels_test = train_test_split(
+        data, labels, train_size=train_ratio, shuffle=True
+    )
+
+    data_train = torch.tensor(citrate_spectra)
+    labels_train = torch.tensor(vinyl_spectra)
+
+    print(data_train.shape)
+    print(labels_train.shape)
+    return data_test, data_train, labels_test, labels_train, torch
+
+
+@app.cell
+def _(
+    data_test,
+    data_train,
+    labels_test,
+    labels_train,
+    loss_fn,
+    model,
+    np,
+    optimizer,
+    torch,
+):
+    n_epochs = 100   # number of epochs to run
+    batch_size = 10  # size of each batch
+    batch_start = torch.arange(0, len(data_train), batch_size)
+
+    import tqdm
+    import copy
+
+    best_mse = np.inf   # init to infinity
+    best_weights = None
+    history = []
+
+    for epoch in range(n_epochs):
+        model.train()
+        with tqdm.tqdm(
+            batch_start, unit='batch', mininterval=0, disable=True
+        ) as bar:
+            bar.set_description(f'Epoch {epoch}')
+            for start in bar:
+                # take a batch
+                data_batch = data_train[start : start + batch_size]
+                labels_batch = labels_train[start : start + batch_size]
+                # forward pass
+                labels_pred = model(data_batch)
+                loss = loss_fn(labels_pred, labels_batch)
+                # backward pass
+                optimizer.zero_grad()
+                loss.backward()
+                # update weights
+                optimizer.step()
+                # print progress
+                bar.set_postfix(mse=float(loss))
+        # evaluate accuracy at end of each epoch
+        model.eval()
+        labels_pred = model(data_test)
+        mse = loss_fn(labels_pred, labels_test)
+        mse = float(mse)
+        history.append(mse)
+        if mse < best_mse:
+            best_mse = mse
+            best_weights = copy.deepcopy(model.state_dict())
     return
 
 
