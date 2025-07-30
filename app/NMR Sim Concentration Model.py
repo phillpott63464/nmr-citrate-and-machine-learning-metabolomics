1b689fc6d461b650de4c93a1a4401e59970c0ddc
sp970 <sp970@kent.ac.uk>
2025-07-30 10:43:28 +0100

    Problem: Morgan's code scales the DSS peak randomly along with the citrate peaks, giving weird integrals, I assume

--- app/NMR Sim Concentration Model.py
+++ app/NMR Sim Concentration Model.py
@@ -28,102 +28,6 @@ def _(mo):
     return
 
 
-@app.cell
-def _():
-    from nmrsim import SpinSystem
-    from nmrsim.math import normalize_peaklist as normalize
-    from nmrsim.plt import mplplot
-
-    def spectrum_creator(analytes):
-        """
-        Inputs:
-            analytes: [
-                {
-                    'v': array, frequencies in Hz
-                    'j': array, matrix of coupling constants
-                    'protons': int, number of contributing protons
-                    'concentration': float, concentration relative to each other
-                }
-            ]
-
-        Outputs:
-            x, y arrays for plotting
-        """
-
-        all_peaklists = []
-        for analyte in analytes:
-            system = SpinSystem(analyte['v'], analyte['j'])
-            normalized = normalize(system.peaklist(), n=analyte['protons'])
-            scaled = [(f, i * analyte['concentration']) for f, i in normalized]
-            all_peaklists.extend(scaled)
-
-        # Now convert the combined peaklist to plotting format
-        plots = mplplot(all_peaklists, hidden=True)
-        return plots
-
-    return (spectrum_creator,)
-
-
-@app.cell(hide_code=True)
-def _(mo):
-    mo.md(r"""Now, try and create and plot citrate via this""")
-    return
-
-
-@app.cell
-def _():
-    import numpy as np
-
-    def createcitrate():
-        v = np.array(
-            [np.mean([2.95, 2.91]) * 600, np.mean([2.77, 2.73]) * 600]
-        )
-        J = np.zeros((2, 2))
-        J[0, 1] = 15.89 / 400 * 600
-        J[1, 0] = 15.89 / 400 * 600
-        return v, J
-
-    def createdss():
-        """Create DSS reference peak at 0 ppm (0 Hz at any field strength)"""
-        v = np.array([0.0])  # DSS peak at 0 Hz
-        J = np.zeros((1, 1))  # No coupling for DSS
-        return v, J
-
-    return createcitrate, createdss, np
-
-
-@app.cell
-def _(createcitrate, createdss, np, spectrum_creator):
-    import matplotlib.pyplot as plt
-    # Create the analytes data
-    v_dss, j_dss = createdss()
-    v_citrate, j_citrate = createcitrate()
-
-    basespectrum = spectrum_creator(
-        analytes=[
-            {
-                'v': v_dss,
-                'j': j_dss,
-                'protons': 9,
-                'concentration': 1.0
-            },
-            {
-                'v': v_citrate,
-                'j': j_citrate,
-                'protons': 4,
-                'concentration': 0.1
-            }
-        ]
-    )
-
-    print(np.array(basespectrum).shape)
-
-    plt.title('Base spectrum')
-    plt.plot(basespectrum[0], basespectrum[1])
-
-    return j_citrate, j_dss, plt, v_citrate, v_dss
-
-
 @app.cell(hide_code=True)
 def _(mo):
     mo.md(r"""Now, create multiple of these spectra with varying concentrations""")
@@ -131,48 +35,48 @@ def _(mo):
 
 
 @app.cell
-def _(j_citrate, j_dss, np, spectrum_creator, v_citrate, v_dss):
-    count = 1000
+def _():
+    from morgan.createTrainingData import createTrainingData
+    import numpy as np
+
+    substanceDict = {
+        'Citric acid': ['SP:3368'],
+    }
+
+    substanceSpectrumIds = [substanceDict[substance][-1] for substance in substanceDict]
+    count = 10
 
     labels = []
     spectra = []
 
     for spectracounter in range (0, count):
         print(f'{spectracounter}/{count}')
-        labels.append(np.random.uniform(0.5, 0.01))
-        spectra.append(spectrum_creator(
-            analytes=[
-                {
-                    'v': v_dss,
-                    'j': j_dss,
-                    'protons': 9,
-                    'concentration': 1
-                },
-                {
-                    'v': v_citrate,
-                    'j': j_citrate,
-                    'protons': 4,
-                    'concentration': labels[spectracounter]
-                }
-            ]
+        spectra.append(createTrainingData(
+            substanceSpectrumIds=substanceSpectrumIds,
+            rondomlyScaleSubstances=True,
         ))
 
-    return labels, spectra
+    print(spectra[0]['scales'])
+    print(spectra[0]['intensities'].shape)
+
+    return labels, np, spectra
 
 
 @app.cell
-def _(labels, plt, spectra):
+def _(labels, spectra):
+    import matplotlib.pyplot as plt
+
     print(len(spectra))
     print(len(labels))
 
-    graph_count = 4
+    graph_count = 3
 
     plt.figure(figsize=(graph_count*4, graph_count*4))
 
     for graphcounter in range(1, graph_count**2+1):
         plt.subplot(graph_count, graph_count, graphcounter)
-        plt.title(f'{round(labels[graphcounter], 3)}M')
-        plt.plot(spectra[graphcounter][0], spectra[graphcounter][1])
+        # plt.title(f'{round(labels[graphcounter], 3)}M')
+        plt.plot(spectra[graphcounter]['positions'], spectra[graphcounter]['intensities'][0])
 
     plt.show()
     return
---
+++ app/morgan/createTrainingData.py
@@ -0,0 +1,129 @@
+import os
+import random
+import pandas as pd
+import numpy as np
+from pathlib import Path
+from datetime import datetime
+from tqdm import tqdm
+from .trainingDataLib import createLineshape, peakListFromSpinSystemMatrix
+
+referenceData = pd.DataFrame({'multiplets': [pd.DataFrame({'center': 0.0, 'scale': 1.0, 'ssm_indices': [[0, 1, 2, 3, 4, 5, 6, 7, 8]]}), None, None],
+                              'ssm'       : [np.zeros([9, 9]), None, None],
+                              'offset'    : [0.0, 0.0, 0.0]},
+                             index=['tsp', 'dss', 'tms'])
+
+
+"""Establish the simulation data sources"""
+dirPath = Path(os.path.dirname(os.path.realpath(__file__)))
+metabolites = pd.read_csv(dirPath.joinpath('Casmdb_Data/metabolites.csv'), index_col=0)
+samples = pd.read_csv(dirPath.joinpath('Casmdb_Data/samples.csv'), index_col=0)
+spectra = pd.read_csv(dirPath.joinpath('Casmdb_Data/spectra.csv'), index_col=0)
+multiplets = pd.read_csv(dirPath.joinpath('Casmdb_Data/multiplets.csv'), index_col=0)
+couplings = pd.read_csv(dirPath.joinpath('Casmdb_Data/couplings.csv'), index_col=0)
+
+
+def createTrainingData(substanceSpectrumIds=[], sampleNumber=1, frequency=600.00, points=2**15, limits=(10, -2),
+                       highLimits=(9, 11), lowLimits=(-1, -3), peakWidth=1.5, scale=1.0,
+                       rondomlyScaleSubstances=True, includeNoise=True, noiseRange=(-2,-3), referenceSubstanceSpectrumId='tsp',
+                       randomlyOffsetMultiplets=True, multipletOffsetCap = 0.01):
+    """
+        Training data and annotations for a ML model.
+        ----------
+        substanceSpectrumIds : [str...]
+            A list of strings for collecting metabolite sims from the database.
+        sampleNumber : int
+            Number of sample spectra to be simulated.
+        frequency : float or int
+            Spectrometer frequency to simulate at.
+        points : int
+            Number of data points.
+        limits : (float, float)
+            Frequency limits for the plot.
+        peakWidth: float or int
+            Peak widths in Hz.
+        scale: float or int
+            Global scaling factor for all the simulated sample spectra.
+        rondomlyScaleSubstances: bool
+            Flag to determine whether to randomise component metabolite spectra scales.
+        includeNoise: bool
+            Flag to determine whether to add an array of noise to the sample simulations.
+        noiseRange: (float or int, float or int)
+            A pair of values for the upper and lower powers for the noise range.
+        referenceSubstanceSpectrumId: str
+            ID of the reference compound.
+        randomlyOffsetMultiplets: bool
+            Flag to determine whether to apply a random offset to the multiplets of the component metabolite spectra.
+        multipletOffsetCap: float or int
+             The absolute value for the maximum multiplet offset in the component metabolite spectra.
+        Returns
+        -------
+        scales, positions, intensities, components: pandas.DataFrame, numpy.array, numpy.array, numpy.array
+            Data on the scales (aka true values of the components), position array of all spectra (x-axis), intensity arrays of the simulated samples (y-axes), intensity arrays of the simulated components before any transformations (y-axes).
+        """
+    scalesDict = {ssid: [scale * (10 ** random.uniform(0.0, 1.0)) if rondomlyScaleSubstances else scale for samNum in range(sampleNumber)] for ssid in substanceSpectrumIds + [referenceSubstanceSpectrumId]}
+    untransformedComponentsList = []
+    transformedComponentsList = []
+    intensitiesList = []
+    peakWidth = peakWidth / frequency
+    for sampleNumber in tqdm(range(sampleNumber)):
+        # Make the reference signal first
+        positions, y = generateSignal(referenceData.loc[referenceSubstanceSpectrumId, 'ssm'], peakWidth, frequency, points, limits, scalesDict[referenceSubstanceSpectrumId][sampleNumber])
+        for spectrumId in substanceSpectrumIds:
+            if sampleNumber == 0:
+                ssm = getSsmData(spectrumId=spectrumId, referenceOffset=referenceData.loc[referenceSubstanceSpectrumId, 'offset'], transform=False, multipletOffsetCap=multipletOffsetCap)
+                x, substanceY = generateSignal(ssm, peakWidth, frequency, points, limits, scalesDict[spectrumId][sampleNumber])
+                untransformedComponentsList.append(substanceY)
+            ssm = getSsmData(spectrumId=spectrumId, referenceOffset=referenceData.loc[referenceSubstanceSpectrumId, 'offset'], transform=randomlyOffsetMultiplets, multipletOffsetCap=multipletOffsetCap)
+            x, substanceY = generateSignal(ssm, peakWidth, frequency, points, limits, scalesDict[spectrumId][sampleNumber])
+            # transformedComponentsList.append(substanceY)
+            y += substanceY
+        if includeNoise:
+            noise = 10**random.uniform(noiseRange[0], noiseRange[-1])
+            y += np.random.normal(y, scale * noise)
+        intensitiesList.append(y)
+    return {
+        'scales': pd.DataFrame(scalesDict),
+        'positions': positions,
+        'intensities': np.vstack(intensitiesList), 
+        'components': np.vstack(untransformedComponentsList)
+        }
+
+
+def generateSignal(ssm, peakWidth, frequency, points, limits, scale):
+    peakList = peakListFromSpinSystemMatrix(ssm, frequency, peakWidth)
+    x, y = createLineshape(peakList, points=points, limits=limits)
+    return x, y * scale
+
+
+def getSsmData(spectrumId, referenceOffset, transform, multipletOffsetCap):
+    multipletsData = multiplets.loc[multiplets['spectrum_id'] == spectrumId]
+    couplingsData = couplings.loc[couplings['spectrum_id'] == spectrumId]
+    ssm = np.zeros((len(multipletsData), len(multipletsData)))
+    for index, row in couplingsData.iterrows():
+        ssm[multipletsData.index.get_loc(row['multiplet_id1']), multipletsData.index.get_loc(row['multiplet_id2'])] = float(row['j'])
+    ssm = ssm + ssm.T
+    for index, center in enumerate(multipletsData['center']):
+        chemShift = float(center) + referenceOffset + random.uniform(-multipletOffsetCap, multipletOffsetCap) if transform else float(center) + referenceOffset
+        ssm[index, index] = float(chemShift)
+    return ssm
+
+
+def createSpinSystemMatrix(splitting_data, multiplet_data, frequency):
+    shifts = [float(shift) * frequency for shift in multiplet_data.center]
+    dimension = len(multiplet_data.index)
+    ss_matrix = np.zeros((dimension, dimension))
+    for cp_index, line in splitting_data.iterrows():
+        j = float(line.j)
+        x = int(line.multiplet_id1.split('.')[-1]) - 1
+        y = int(line.multiplet_id2.split('.')[-1]) - 1
+        # x = multiplet_data.index[multiplet_data['multiplet_id'] == line.multiplet_id1].tolist()[0]
+        # y = multiplet_data.index[multiplet_data['multiplet_id'] == line.multiplet_id2].tolist()[0]
+        ss_matrix[x, y] = j
+    ss_matrix = ss_matrix + ss_matrix.T
+    # if len(ss_matrix) > 11:
+    #     print('spin system matrix is too large')
+    #     return None
+    for index, shift in enumerate(shifts):
+        ss_matrix[index][index] = round(shift / frequency, 4)
+    return ss_matrix
+
