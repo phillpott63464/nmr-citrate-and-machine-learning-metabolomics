b61c89d20b7a45c6f84a0c35994d438cab029815
phillpott63464 <samuel@phillpott.uk>
2025-09-03 16:07:13 +0100

    Added morgan's dataset

---
+++ app/View Morgans Spectra.py
@@ -0,0 +1,136 @@
+import marimo
+
+__generated_with = "0.15.2"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    import marimo as mo
+
+    import os
+    import re
+    import numpy as np
+    return np, os, re
+
+
+@app.cell
+def _(np, os, re):
+    def extract_phc(experiment, experiment_number, data_dir):
+        """Extract Phase Correction values from the procs file."""
+
+        dir_path = f'{data_dir}/{experiment}/{experiment_number}'
+
+        if not os.path.exists(dir_path):
+            raise FileNotFoundError(f'Directory {dir_path} does not exist')
+
+        procs_file_path = f'{dir_path}/pdata/1/procs'
+        if not os.path.exists(procs_file_path):
+            raise FileNotFoundError(
+                f'Directory {dir_path} does not contain procs parameters'
+            )
+
+        phc = {}
+
+        with open(procs_file_path) as f:
+            for line in f:
+                match = re.search(r'\$PHC(\d*)=\s*([-+]?\d*\.\d+|\d+)', line)
+                if match:
+                    key = f'PHC{match.group(1)}' if match.group(1) else 'PHC'
+                    phc[key] = np.float64(match.group(2))
+
+        if not phc:
+            raise ValueError(
+                f'No phase correction values in directory {dir_path}'
+            )
+
+        return phc
+
+    def bruker_fft(data_dir, experiment, experiment_number):
+        """
+        Convert time domain data to frequency domain
+        https://github.com/jjhelmus/nmrglue/blob/master/examples/bruker_processed_1d/bruker_processed_1d.py
+        """
+        import nmrglue as ng
+
+        phc = extract_phc(
+            data_dir=data_dir,
+            experiment_number=experiment_number,
+            experiment=experiment,
+        )
+
+        data, dic = read_bruker(data_dir, experiment, experiment_number)
+        # data = read_fid(data_dir=data_dir, experiment=experiment, experiment_number=experiment_number)
+
+        # Process the spectrum
+        # data = ng.proc_base.zf_size(
+        #     data, 2**20
+        # )    # Zero fill to 32768 points
+        data = ng.proc_base.fft(data)                 # Fourier transform
+        data = ng.proc_base.ps(
+            data, p0=phc['PHC0'], p1=phc['PHC1']
+        )  # Phase correction
+        # data = ng.proc_autophase.autops(
+        #     data, 'peak_minima', p0=phc['PHC0'], p1=phc['PHC1']
+        # )   # Automatic phase correction
+        data = ng.proc_base.di(data)                  # Discard the imaginaries
+        data = ng.proc_base.rev(data)                 # Reverse the data=
+
+        udic = ng.bruker.guess_udic(dic, data)
+        uc = ng.fileiobase.uc_from_udic(udic)
+        ppm_scale = uc.ppm_scale()
+
+        return ppm_scale, data
+
+    def read_bruker(data_dir, experiment, experiment_number):
+        import nmrglue as ng
+
+        dic, data = ng.bruker.read(
+            f'{data_dir}/{experiment}/{experiment_number}'
+        )
+        # Remove the digital filter
+        data = ng.bruker.remove_digital_filter(dic, data)
+
+        return data, dic
+    return (bruker_fft,)
+
+
+@app.cell
+def _():
+    data_dir = 'Morgan-Spectra/2023-02-16_Synthetic_Mixtures'   # The directory all data is in
+
+    import pandas as pd
+
+    file = pd.ExcelFile('Morgan-Spectra/Synthetic_Mixture_Lookup.xlsx')
+
+    spectra_sheet = pd.read_excel(file,'Spectra')
+
+    indices = [x for x, y in zip(spectra_sheet['spectrumFileName'], spectra_sheet['experimentType']) if y == 'H']
+
+    return data_dir, indices
+
+
+@app.cell
+def _():
+    return
+
+
+@app.cell
+def _(bruker_fft, data_dir, indices):
+    from matplotlib import pyplot as plt
+
+    data = {}
+    for experiment in indices:
+        print(experiment)
+        data[experiment] = bruker_fft(data_dir, experiment, experiment_number='')
+    return data, plt
+
+
+@app.cell
+def _(data, plt):
+    plt.plot(*data[294])
+    return
+
+
+if __name__ == "__main__":
+    app.run()
