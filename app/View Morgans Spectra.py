08797193c12e1771d46b4023f70e41c27da64eed
phillpott63464 <samuel@phillpott.uk>
2025-09-05 15:04:24 +0100

    Added ranging to morgan's spectra, and some rudimentary peak picking

--- app/View Morgans Spectra.py
+++ app/View Morgans Spectra.py
@@ -11,11 +11,12 @@ def _():
     import os
     import re
     import numpy as np
-    return np, os, re
+    import nmrglue as ng
+    return mo, ng, np, os, re
 
 
 @app.cell
-def _(np, os, re):
+def _(ng, np, os, re):
     def extract_phc(experiment, experiment_number, data_dir):
         """Extract Phase Correction values from the procs file."""
 
@@ -51,7 +52,7 @@ def _(np, os, re):
         Convert time domain data to frequency domain
         https://github.com/jjhelmus/nmrglue/blob/master/examples/bruker_processed_1d/bruker_processed_1d.py
         """
-        import nmrglue as ng
+
 
         phc = extract_phc(
             data_dir=data_dir,
@@ -67,12 +68,12 @@ def _(np, os, re):
         #     data, 2**20
         # )    # Zero fill to 32768 points
         data = ng.proc_base.fft(data)                 # Fourier transform
-        data = ng.proc_base.ps(
-            data, p0=phc['PHC0'], p1=phc['PHC1']
-        )  # Phase correction
-        # data = ng.proc_autophase.autops(
-        #     data, 'peak_minima', p0=phc['PHC0'], p1=phc['PHC1']
-        # )   # Automatic phase correction
+        # data = ng.proc_base.ps(
+        #     data, p0=phc['PHC0'], p1=phc['PHC1']
+        # )  # Phase correction
+        data = ng.proc_autophase.autops(
+            data, 'peak_minima', p0=phc['PHC0'], p1=phc['PHC1']
+        )   # Automatic phase correction
         data = ng.proc_base.di(data)                  # Discard the imaginaries
         data = ng.proc_base.rev(data)                 # Reverse the data=
 
@@ -106,7 +107,6 @@ def _():
     spectra_sheet = pd.read_excel(file,'Spectra')
 
     indices = [x for x, y in zip(spectra_sheet['spectrumFileName'], spectra_sheet['experimentType']) if y == 'H']
-
     return data_dir, indices
 
 
@@ -116,19 +116,72 @@ def _():
 
 
 @app.cell
-def _(bruker_fft, data_dir, indices):
+def _(bruker_fft, data_dir, indices, np):
     from matplotlib import pyplot as plt
 
     data = {}
     for experiment in indices:
-        print(experiment)
-        data[experiment] = bruker_fft(data_dir, experiment, experiment_number='')
+        data[experiment] = np.array(bruker_fft(data_dir, experiment, experiment_number=''))
+
+        if max(data[experiment][1]) < min(data[experiment][1]):
+            data[experiment][1] = data[experiment][1] * -1
+
+        lower_bound, upper_bound = 2.4, 2.8
+
+        x = data[experiment][0]
+
+        mask = (x >= lower_bound) & (x <= upper_bound)
+    
+        data[experiment] = data[experiment][:, mask]
     return data, plt
 
 
 @app.cell
-def _(data, plt):
-    plt.plot(*data[294])
+def _(plt):
+    def gen_fig(data, peaks=None):
+        figure = plt.figure()
+        plt.plot(*data)
+
+        if peaks is not None:
+            scatterx, scattery = peaks[:, 0], peaks[:, 1]
+            plt.scatter(scatterx, scattery, color='RED')
+        return figure
+    return (gen_fig,)
+
+
+@app.cell
+def _(data, gen_fig, ng, np):
+    figs = {}
+    peaks = {}
+
+    for set_name in data:
+        arr = data[set_name]          # shape (2, 32000)
+        y = arr[1]
+        threshold = np.percentile(y, 99)
+
+        ymask = (y >= threshold)
+
+        # peaks[set_name] = data[set_name][:, ymask]
+
+        # print(len(peaks[set_name][0]))
+
+        peak_positions = [x[1] for x in ng.analysis.peakpick.pick(data[set_name], threshold)]
+        peak_positions = [data[set_name][:, int(i)] for i in peak_positions]
+        peak_positions = np.vstack(peak_positions)
+        peaks[set_name] = peak_positions
+    
+        figs[set_name] = gen_fig(data[set_name], peaks[set_name])
+
+        # if len(figs) > 0:
+        #     die
+
+    # Now you can show any figure later using figs[index]
+    return (figs,)
+
+
+@app.cell
+def _(figs, mo):
+    mo.mpl.interactive(figs[34])
     return
 
 
